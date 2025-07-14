1395fd9442fef5f91e3006aeccdbdea14c8c4ffc
OigenD <97177749+OigenD@users.noreply.github.com>
2025-07-14 14:57:19 +0300

    Update sonar_mapping.py

--- sonar_mapping.py
+++ sonar_mapping.py
@@ -17,23 +17,14 @@ def run_command(command, output_file=None):
         raise
 
 def main():
-    # Get ArgoCD and GitLab credentials from environment variables
-    argocd_server = os.getenv("ARGOCD_SERVER")
-    argocd_username = os.getenv("ARGOCD_USERNAME")
-    argocd_password = os.getenv("ARGOCD_PASSWORD")
+    # Get GitLab token from environment variable
     gitlab_token = os.getenv("GITLAB_TOKEN")
-
-    # Check if required environment variables are set
-    if not all([argocd_server, argocd_username, argocd_password, gitlab_token]):
-        raise ValueError("Missing required environment variables: ARGOCD_SERVER, ARGOCD_USERNAME, ARGOCD_PASSWORD, or GITLAB_TOKEN")
+    if not gitlab_token:
+        raise ValueError("Missing required environment variable: GITLAB_TOKEN")
 
     # Create tmp directory if it doesn't exist
     os.makedirs("tmp", exist_ok=True)
     
-    # Authenticate with ArgoCD
-    argocd_login_cmd = f"argocd login {argocd_server} --username {argocd_username} --password {argocd_password} --grpc-web"
-    run_command(argocd_login_cmd)
-
     # Define timestamp for dynamic file names
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M")
     
@@ -44,7 +35,6 @@ def main():
         ("kubectl get svc -A", "tmp/experiment-svc.txt"),
         ("kubectl get all --all-namespaces -o yaml", "tmp/cluster-state-before.yaml"),
         ("kubectl get events --all-namespaces", "tmp/events-before.txt"),
-        ("argocd-linux-amd64 app list -o json --grpc-web", "tmp/argocd-apps-state-before.json"),
         ("kubectl get applications.argoproj.io -A -o yaml", "tmp/argocd-applications.yaml"),
         (f"kubectl get nodes,pods,services,deployments,statefulsets,daemonsets,replicasets,jobs,cronjobs --all-namespaces -o wide", f"tmp/cluster-state-{timestamp}.txt"),
         (f"kubectl get all,configmap,secret,ingress,storageclass,persistentvolume,persistentvolumeclaim,namespace,role,rolebinding,clusterrole,clusterrolebinding,serviceaccount --all-namespaces -o yaml", f"tmp/all-manifests-{timestamp}.yaml")
@@ -59,6 +49,8 @@ def main():
     git_commands = [
         "git init --initial-branch=main",
         f"git remote add origin {git_repo_url} || git remote set-url origin {git_repo_url}",
+        "git config user.name 'Cluster Admin'",
+        "git config user.email 'admin@example.com'",
         "git pull --rebase origin main",
         "git add tmp/*",
         f'git commit -m "Добавлены файлы состояния кластера {timestamp}" || true',
